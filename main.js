523771fd874a6d6ea72fd67f594d6c2c70a880cd
Kazuki Ino <kazuki.ino01@gmail.com>
2025-12-22 11:46:30 +0900

    不要なコメントを整理

--- main.js
+++ main.js
@@ -6,27 +6,6 @@ document.addEventListener("DOMContentLoaded", () => { // Documet(HTML) Object Mo
 
   let posts = []; // 投稿を格納する配列
 
-  // // 【LocalStrage版】初期化：LocalStorageからデータを読み込む
-  // function loadPosts() {
-  //   const savedData = localStorage.getItem("live_mate_posts"); // key値："live_mate_posts"
-  //   if (savedData) {
-  //     posts = JSON.parse(savedData);
-
-  //     // 保存されたデータがあれば表示する
-  //     if (posts.length > 0) {
-  //       if (emptyMessage) {
-  //         emptyMessage.remove();
-  //       }
-  //       // 配列は新しい順（[0]が最新）なので、順番にappendChildしていけばok
-  //       // 今回はunshiftで追加しているので[0]が最新
-  //       posts.forEach((post) => {
-  //         const item = createPostElement(post);
-  //         postList.appendChild(item);
-  //       });
-  //     }
-  //   }
-  // }
-
   // 【JSONサーバー版】初期化：LocalStorageからデータを読み込む
   async function loadPosts() { // asyncによりawaitが使用可能になる
       try {
@@ -48,11 +27,6 @@ document.addEventListener("DOMContentLoaded", () => { // Documet(HTML) Object Mo
           alert("サーバーから読み込めなかった（json-server起動してる？）");
       }
   }
-
-  // // 【LocalStrage版】投稿データをLocalStorageに保存する
-  // function savePosts() {
-  //   localStorage.setItem("live_mate_posts", JSON.stringify(posts)); // JSON文字列で保存
-  // }
 
   // 投稿のHTML要素を作成する関数
   function createPostElement(post) {
@@ -85,14 +59,7 @@ document.addEventListener("DOMContentLoaded", () => { // Documet(HTML) Object Mo
     return item;
   }
 
-  // // 【LocalStrage版】指定したIDのポスを削除する関数
-  // function deletePost(id) {
-  //   // 指定したID以外のものを残す（＝指定したIDを消す）
-  //   posts = posts.filter((p) => p.id !== id);
-  //   savePosts();
-  // }
-
-  // 【JSONサーバー版】指定したIDのポスを削除する関数
+  // 【JSONサーバー版】指定したIDのポストを削除する関数
   async function deletePost(id) {
       try {
           await fetch(`http://localhost:3001/posts/${id}`, {
@@ -109,52 +76,6 @@ document.addEventListener("DOMContentLoaded", () => { // Documet(HTML) Object Mo
 
   // 起動時に読み込みを実行
   loadPosts();
-
-  // // 【LocalStrage版】フォーム送信時のイベント
-  // form.addEventListener("submit", (event) => {
-  //   event.preventDefault(); // 画面リロードを止める
-
-  //   // 入力値を取得
-  //   const liveName = document.getElementById("title").value.trim();
-  //   const liveDate = document.getElementById("date").value.trim();
-  //   const liveArea = document.getElementById("area").value.trim();
-  //   const liveNote = document.getElementById("comment").value.trim();
-
-  //   // かんたんなバリデーション
-  //   if (!liveName || !liveDate || !liveArea) {
-  //     alert("ライブ名・日付・会場は入力してね！");
-  //     return;
-  //   }
-
-  //   //新しい投稿をオブジェクトとして整理
-  //   const newPost = {
-  //     id: Date.now(), // ユニークなIDを追加
-  //     title: liveName,
-  //     date: liveDate,
-  //     area: liveArea,
-  //     comment: liveNote,
-  //   };
-
-  //   //新しいポストを配列に追加 (先頭に追加)
-  //   posts.unshift(newPost);
-
-  //   // 保存
-  //   savePosts();
-
-  //   // 「まだ投稿はありません。」を消す
-  //   if (emptyMessage) {
-  //     emptyMessage.remove(); // DOMから削除
-  //   }
-
-  //   // 募集カードを作って表示
-  //   const item = createPostElement(newPost);
-
-  //   // 一番上に追加
-  //   postList.prepend(item);
-
-  //   // フォームをリセット
-  //   form.reset();
-  // });
 
   // 【JSONサーバー版】フォーム送信時のイベント
   form.addEventListener("submit", async (event) => {
